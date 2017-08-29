e2ea69071735cf7d6609e5e72991f7a36e20691c
EgoFelix <git@egofelix.de>
2017-08-29 19:25:08 +0200

    use root user

--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 
 if [ ! -d "/run/mysqld" ]; then
 	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
+	chown -R root:root /run/mysqld
 fi
 
 if [ ! -d /var/lib/mysql/mysql ]; then
 	echo "[i] MySQL data directory not found, creating initial DBs"
 
-	chown -R mysql:mysql /var/lib/mysql
+	chown -R root:root /var/lib/mysql
 
-	mysql_install_db --user=mysql > /dev/null
+	mysql_install_db --user=root > /dev/null
 
 	if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
 	    apk --no-cache add pwgen
@@ -32,9 +32,9 @@ GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' identified by 'MYSQL_ROOT_PASS
 UPDATE user SET password=PASSWORD("") WHERE user='root' AND host='localhost';
 EOF
 
-	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < $tfile
+	/usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tfile
 	rm -f $tfile
 fi
 
 echo "[i] Starting MySQL"
-exec /usr/bin/mysqld --user=mysql --console
+exec /usr/bin/mysqld --user=root --console
